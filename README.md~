8ecf969a243274ab50c98aa2a73e76305dc21822
Philip <pille72@gmail.com>
2014-05-10 20:06:05 +0200

    Rename folder structure

---
+++ README.md~
@@ -0,0 +1,1 @@
+### Just some C# programs for learning
--- c#/GPI11A/HalloWelt.cs
+++
@@ -1,6 +0,0 @@
-using System;
-public class HalloWelt {
-	public static void Main(string [] args) {
-		Console.WriteLine("Hallo Welt");
-	}
-}
